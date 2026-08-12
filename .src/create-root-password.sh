e1e6c37b81d3218afff5dc361d8fd2f9054e6144
Simon Walz <contact@simonwalz.de>
2026-08-13 00:02:16 +0200

    refact: root-password: more options

--- .src/create-root-password.sh
+++ .src/create-root-password.sh
@@ -5,6 +5,13 @@ show_help() {
   echo
   echo "Without arguments: Enter password and show hash."
   echo "-w system.json  - write password to json file"
+  echo "-g              - generate random password"
+  # field: os_root_password
+  echo "-r              - password for os_root_password"
+  # field: console_password
+  echo "-c              - password for console_password"
+  # field: console_expert_password
+  echo "-e              - password for console_expert_password"
 }
 
 jq_set() {
@@ -19,48 +26,73 @@ jq_set() {
   echo "${new_config}" >"${file}"
 }
 
-password_hint() {
-  echo "Please enter a password:"
-  echo "Please be aware, that you might need to enter it via an english keyboard on a console within 30secs."
+generate_password() {
+  local L="${PASSWORD_LENGTH:-"20"}"
+  {
+    export LC_CTYPE=C
+    export LC_ALL=C
+    export LANG=C
 
-  if command -v openssl >/dev/null 2>&1; then
-    {
-      local L="${PASSWORD_LENGTH:-"20"}"
-      export LC_CTYPE=C
-      export LC_ALL=C
-      export LANG=C
-
-      root_password="$(openssl rand "$((L*8))" | tr -dc 'A-Xa-x0-9!$%' | fold -w "$L" | head -n1)"
-      echo "Random: ${root_password}"
-    }
-  fi
-
+    root_password="$(openssl rand "$((L*8))" | tr -dc 'A-Xa-x0-9!$%' | fold -w "$L" | head -n1)"
+    echo "${root_password}"
+  }
 }
 
-if [[ "${1:-}" == "-h" || "${1:-}" == "--help" ]]; then
-  show_help
-  exit 2
-fi
+password_hint() {
+  echo "Please be aware, that you might need to enter it via an english keyboard on a console within 30secs."
 
-if command -v openssl >/dev/null 2>&1; then
-  password_hint
-  root_hash="$(openssl rand -base64 12 | tr '+/' './' | cut -c1-16)"
-  root_hash="$(openssl passwd -6 -salt "$root_hash")"
-else
+  echo "Password Suggestion: $(generate_password)"
+
+  echo "Please enter a password:"
+}
+
+key="os_root_password"
+generate_password=0
+system_config=""
+
+while getopts "rcegw:h" opt; do
+  case "$opt" in
+    r) key="os_root_password" ;;
+    c) key="console_password" ;;
+    e) key="console_expert_password" ;;
+    g) generate_password=1 ;;
+    w) system_config="$OPTARG" ;;
+    h) show_help; exit 2 ;;
+    *) show_help; exit 2 ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if ! command -v openssl >/dev/null 2>&1; then
   echo "Needed tools are not availbale Please install openssl" >&2
   exit 1
 fi
-unset root_password
 
-if [[ "${1:-}" == "-w" && -n "${2:-}" && -f "${2}" ]]; then
-  system_config="${2}"
+if [[ "${generate_password}" == "1" ]]; then
+  root_password="$(generate_password)"
+  echo "${key}: ${root_password}"
+  root_salt="$(openssl rand -base64 12 | tr '+/' './' | cut -c1-16)"
+  root_hash="$(echo -n "${root_password}" | openssl passwd -6 -salt "$root_salt" -stdin)"
+
+  unset root_password
+else
+  password_hint
+  root_salt="$(openssl rand -base64 12 | tr '+/' './' | cut -c1-16)"
+  root_hash="$(openssl passwd -6 -salt "$root_salt")"
+fi
+
+
+if [[ -n "${system_config}" ]]
+then
   jq_set \
+    --arg key "${key}" \
     --arg password "${root_hash}" \
-    '.os_root_password = $password' \
+    '.[$key] = $password' \
     "${system_config}"
 else
-  # show password
   jq -n \
+    --arg key "${key}" \
     --arg password "${root_hash}" \
-    '{"os_root_password": $password}'
+    '.[$key] = $password'
 fi
+
